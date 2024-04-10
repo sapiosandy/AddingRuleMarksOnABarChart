27a0b7ee732a4d3eb601b2fd6a2ab5cd503c9b45
Sandy Gomez <sgomez1029@gmail.com>
2024-04-10 12:21:02 -0700

    Adding Rule Marks on a Bar Chart (UCSD Extension)

--- AddingRuleMarksOnABarChart/ContentView.swift
+++ AddingRuleMarksOnABarChart/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SalesPeople: Identifiable {
+    var name: String
+    var sales: Int
+    var id = UUID()
+}
+
+var mySalesArray: [SalesPeople] = [
+    .init(name: "Oscar", sales: 50),
+    .init(name: "Meyer", sales: 43),
+    .init(name: "George", sales: 62),
+    .init(name: "Hormel", sales: 26)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart (mySalesArray) { z in
+            BarMark(x: .value("Name", z.name),
+                    y: .value("Sales", z.sales))
+            
+            RuleMark(y: .value("Average Sales", (mySalesArray.map{Int($0.sales)}).reduce(0, +) / Set(mySalesArray.map({$0.sales})).count))
+                .foregroundStyle(.green)
+            
+            RuleMark(y: .value("Sales must not go below this line", 20))
+                .foregroundStyle(.red)
         }
-        .padding()
     }
 }
 
