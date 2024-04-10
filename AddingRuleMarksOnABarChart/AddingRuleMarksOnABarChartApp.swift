de9fa99a09b48c2e2a68c4fe7d2fcecb90d358ce
Sandy Gomez <sgomez1029@gmail.com>
2024-04-10 12:06:10 -0700

    Initial Commit

---
+++ AddingRuleMarksOnABarChart/AddingRuleMarksOnABarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingRuleMarksOnABarChartApp.swift
+//  AddingRuleMarksOnABarChart
+//
+//  Created by Sandra Gomez on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AddingRuleMarksOnABarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
